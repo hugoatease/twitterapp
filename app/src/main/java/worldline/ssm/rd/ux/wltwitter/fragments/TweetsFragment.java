44d0aa56391410f62c1a3baa2ed54c6db15daf98
Hugo Caille <hugo.caille@isen-lille.fr>
2015-10-02 16:45:09 +0200

    ArrayAdapter to display tweets

--- app/src/main/java/worldline/ssm/rd/ux/wltwitter/fragments/TweetsFragment.java
+++ app/src/main/java/worldline/ssm/rd/ux/wltwitter/fragments/TweetsFragment.java
@@ -5,9 +5,12 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import worldline.ssm.rd.ux.wltwitter.R;
 import worldline.ssm.rd.ux.wltwitter.WLTwitterApplication;
 import worldline.ssm.rd.ux.wltwitter.http.TweetAsyncTask;
@@ -16,10 +19,14 @@ import worldline.ssm.rd.ux.wltwitter.pojo.Tweet;
 import java.util.List;
 
 public class TweetsFragment extends Fragment implements TweetListener {
+    private ListView tweetsView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tweets, container, false);
+        View view = inflater.inflate(R.layout.fragment_tweets, container, false);
+        this.tweetsView = (ListView) view.findViewById(R.id.tweetsListView);
+        return view;
     }
 
     @Override
@@ -27,11 +34,14 @@ public class TweetsFragment extends Fragment implements TweetListener {
         super.onStart();
         SharedPreferences prefs = WLTwitterApplication.getContext().getSharedPreferences(getString(R.string.login_information), Context.MODE_PRIVATE);
         String login = prefs.getString("login", "");
-        new TweetAsyncTask(this).execute(login);
+        if (!TextUtils.isEmpty(login)) {
+            new TweetAsyncTask(this).execute(login);
+        }
     }
 
     @Override
     public void onTweetsRetrieved(List<Tweet> tweets) {
-
+        final ArrayAdapter<Tweet> adapter = new ArrayAdapter<Tweet>(getActivity(), android.R.layout.simple_list_item_1, tweets);
+        this.tweetsView.setAdapter(adapter);
     }
 }
