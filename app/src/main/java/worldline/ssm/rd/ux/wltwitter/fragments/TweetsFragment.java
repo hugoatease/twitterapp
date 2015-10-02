73c66b6bf5f61839a41217d3275cecdd8b87794c
Hugo Caille <hugo.caille@isen-lille.fr>
2015-10-02 17:09:04 +0200

    Progress feedback for tweets fetching

--- app/src/main/java/worldline/ssm/rd/ux/wltwitter/fragments/TweetsFragment.java
+++ app/src/main/java/worldline/ssm/rd/ux/wltwitter/fragments/TweetsFragment.java
@@ -9,8 +9,7 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.*;
 import worldline.ssm.rd.ux.wltwitter.R;
 import worldline.ssm.rd.ux.wltwitter.WLTwitterApplication;
 import worldline.ssm.rd.ux.wltwitter.http.TweetAsyncTask;
@@ -19,14 +18,15 @@ import worldline.ssm.rd.ux.wltwitter.pojo.Tweet;
 import java.util.List;
 
 public class TweetsFragment extends Fragment implements TweetListener {
+    private View rootView;
     private ListView tweetsView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_tweets, container, false);
-        this.tweetsView = (ListView) view.findViewById(R.id.tweetsListView);
-        return view;
+        this.rootView = inflater.inflate(R.layout.fragment_tweets, container, false);
+        this.tweetsView = (ListView) this.rootView.findViewById(R.id.tweetsListView);
+        return this.rootView;
     }
 
     @Override
@@ -34,6 +34,15 @@ public class TweetsFragment extends Fragment implements TweetListener {
         super.onStart();
         SharedPreferences prefs = WLTwitterApplication.getContext().getSharedPreferences(getString(R.string.login_information), Context.MODE_PRIVATE);
         String login = prefs.getString("login", "");
+
+        final ProgressBar progressBar = new ProgressBar(getActivity());
+        RelativeLayout.LayoutParams layout = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        layout.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        progressBar.setLayoutParams(layout);
+        progressBar.setIndeterminate(true);
+        this.tweetsView.setEmptyView(progressBar);
+        ((ViewGroup) this.rootView).addView(progressBar);
+
         if (!TextUtils.isEmpty(login)) {
             new TweetAsyncTask(this).execute(login);
         }
